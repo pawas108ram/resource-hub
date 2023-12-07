299ba549d8f989386a5ad93a147687e66810ebd0
pawas goyal <pawas48ram@gmail.com>
2023-12-07 21:25:22 +0530

    Changed hover once more

--- components/buttons/AuthSocialButton.tsx
+++ components/buttons/AuthSocialButton.tsx
@@ -9,7 +9,7 @@ interface AuthSocialButtonProps{
 
 const AuthSocialButton:React.FC<AuthSocialButtonProps> = ({icon:Icon,onClick,isGoogle}) => {
   return (
-  <button type='button' onClick={onClick} className={clsx('inline-flex w-4/5  justify-center rounded  px-4 py-1 text-black shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0 ',isGoogle?'bg-red-800 text-white hover:bg-red-400 ':'bg-white')}><Icon size={32}/></button>
+  <button type='button' onClick={onClick} className={clsx('inline-flex w-4/5  justify-center rounded  px-4 py-1 text-black shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0 ',isGoogle?'bg-red-600 text-white hover:bg-red-500 ':'bg-white')}><Icon size={32}/></button>
   )
 }
 
