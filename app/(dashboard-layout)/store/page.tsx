946854ceb7da6b9d8bc799b2614f272989914943
pawas goyal <pawas48ram@gmail.com>
2023-12-07 22:21:26 +0530

    Corrected the stores page

--- app/(dashboard-layout)/store/page.tsx
+++ app/(dashboard-layout)/store/page.tsx
@@ -45,15 +45,15 @@ const StorePage = () => {
 
     
   return (
-      <div className='bg-black/90 w-full max-h-screen text-white flex flex-col items-center p-4 gap-8 overflow-hidden xs:p-2 xs:pb-32 lg:pb-0 '>
+      <div className='bg-black/90 w-full max-h-screen text-white flex flex-col items-center p-4 gap-1 overflow-hidden xs:p-2 xs:pb-32 lg:pb-0 '>
           {currentUser !== null ? <> <Heading body='ResourceHub Store' className='text-white underline' />
-              <div className='flex flex-row  gap-2 items-center justify-end p-2 w-full h-full overflow-hidden '>
-                  <div className="flex flex-col gap-1 outline outline-1 outline-white py-2 px-4 items-center flex-shrink-0 ">
-            <span className='   font-semibold flex flex-row items-center gap-2  '><FaCoins size={20} /> {currentUser.coins} coins</span>
-                        <span className='   font-semibold flex flex-row items-center gap-2'><Image src='/images/pendrive.png' width={18} height={18} alt='pendrive'/>   {currentUser.keys} pendrives</span>
+              <div className='flex flex-row  gap-2 items-center justify-end p-2 w-full  '>
+                  <div className="flex flex-col gap-1 outline outline-1 outline-white py-2 px-4 items-center  ">
+            <span className='   font-semibold flex flex-row items-center gap-2   '><FaCoins size={20} /> {currentUser.coins} coins</span>
+                        <span className='   font-semibold flex flex-row items-center gap-2 '><Image src='/images/pendrive.png' width={18} height={18} alt='pendrive'/>   {currentUser.keys} pendrives</span>
                   </div>
                </div>
-             <div className='grid lg:grid-cols-4 xs:grid-cols-2  gap-2  xs:w-full lg:w-5/6 bg-white/10 rounded p-3 xs:h-full  lg:h-auto lg:overflow-visible xs:overflow-y-auto'>
+             <div className='grid lg:grid-cols-4 xs:grid-cols-2  gap-2  xs:w-full lg:w-5/6 bg-white/10 rounded p-3 h-full overflow-y-auto  '>
                   {StoreProducts.map((product, ele) => (
                       <StoreProduct key={ele} product={product} currentUserCoins={currentUser.coins} />
                   ))}
